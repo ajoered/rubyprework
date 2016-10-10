9be233385fc8f85a4d87278a63cc191edeb797de
Alvarojoered <arojaspen@gmail.com>
2016-10-10 17:46:05 +0200

    Fix spaces issue with Iteration 3 of Caesar’s Cipher

--- caesars_cipher/caesarcipher3.rb
+++ caesars_cipher/caesarcipher3.rb
@@ -1,16 +1,19 @@
 def solve_cipher(input)
 	shift = -3
 input.chars.map do |x|
+	
 	new_value = x.ord + shift
-	if x == " "
-   	new_value = " ".ord - 26
-	end
+
 	if new_value > 122
 		new_value = 97 + new_value - 122 - 1
 	end
 	if new_value < 97
 		new_value = 122 - (97 - new_value) + 1
 	end
+	if x == " "
+   	new_value = " "
+	end
+	
 	new_value.chr
 end.join
 end
