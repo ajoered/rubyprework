25f3266895d257ad853449fce1b6a2266c9c194e
Alvarojoered <arojaspen@gmail.com>
2016-10-10 13:02:08 +0200

    Complete iteration 1 of Caesar’s Cipher including solution for < “a” and > “z” values

--- caesars_cipher/caesarcipher1.rb
+++ caesars_cipher/caesarcipher1.rb
@@ -1,8 +1,15 @@
 def solve_cipher(input)
 shift = -1
-ascii = input.chars.map { |x| x.ord } 
-shifted = ascii.map { |x| x + shift }
-shifted.map { |x| x.chr }.join
+input.chars.map do |x|
+	new_value = x.ord + shift
+	if new_value > 122
+		new_value = 97 + new_value - 122 - 1
+	end
+	if new_value < 97
+		new_value = 122 - (97 - new_value) + 1
+	end
+	new_value.chr
+end.join
 end
 
 puts solve_cipher("ifmmp")
